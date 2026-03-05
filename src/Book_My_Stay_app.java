71fefd658234ed9b754c20b238dce6a84ddc33d0
ignitedreaper <k.manojkumar5608@gmail.com>
2026-03-05 14:59:46 +0530

    UC2: Add room types with static availability

--- src/Book_My_Stay_app.java
+++ src/Book_My_Stay_app.java
@@ -1,12 +1,36 @@
-public class BookMyStayApp {
+abstract class Room {
 
-    public static void main(String[] args) {
+    String type;
+    double price;
 
-        System.out.println("=================================");
-        System.out.println("WELCOME TO BOOK MY STAY APP");
-        System.out.println("Hotel Booking Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("=================================");
+    Room(String type,double price){
+        this.type = type;
+        this.price = price;
+    }
 
+    void display(){
+        System.out.println("Room Type : "+type);
+        System.out.println("Price : "+price);
+    }
+}
+
+class SingleRoom extends Room{
+
+    SingleRoom(){
+        super("Single Room",2000);
+    }
+}
+
+class DoubleRoom extends Room{
+
+    DoubleRoom(){
+        super("Double Room",3500);
+    }
+}
+
+class SuiteRoom extends Room{
+
+    SuiteRoom(){
+        super("Suite Room",7000);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+SingleRoom s = new SingleRoom();
+DoubleRoom d = new DoubleRoom();
+SuiteRoom su = new SuiteRoom();
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
-}
+int singleAvailable = 5;
+int doubleAvailable = 3;
+int suiteAvailable = 2;
+
+s.display();
+System.out.println("Available : "+singleAvailable);
+
+d.display();
+System.out.println("Available : "+doubleAvailable);
+
+su.display();
+System.out.println("Available : "+suiteAvailable);
+
