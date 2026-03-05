a93a074945c9c5814268e87d792d59d0b56ae0c3
ignitedreaper <k.manojkumar5608@gmail.com>
2026-03-05 15:02:16 +0530

    UC6: Implement reservation confirmation and room allocation

--- src/Book_My_Stay_app.java
+++ src/Book_My_Stay_app.java
@@ -1,12 +1,19 @@
-public class BookMyStayApp {
+import java.util.*;
 
-    public static void main(String[] args) {
+class BookingService{
 
-        System.out.println("=================================");
-        System.out.println("WELCOME TO BOOK MY STAY APP");
-        System.out.println("Hotel Booking Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("=================================");
+    HashMap<String,Set<String>> allocatedRooms = new HashMap<>();
 
+    void confirmReservation(Reservation r){
+
+        String roomID = r.roomType.substring(0,2)+"-"+UUID.randomUUID().toString().substring(0,4);
+
+        allocatedRooms.putIfAbsent(r.roomType,new HashSet<>());
+
+        allocatedRooms.get(r.roomType).add(roomID);
+
+        System.out.println("Reservation Confirmed");
+        System.out.println("Guest : "+r.guestName);
+        System.out.println("Room ID : "+roomID);
     }
 }
